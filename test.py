0d20a4a67509c8df889eac88c2450823aeb60afa
jung-geun <jgbong0306@gmail.com>
2022-06-28 16:26:11 +0900

    app edit

--- test.py
+++ test.py
@@ -2,19 +2,32 @@
 """
 Created on Fri Jun 24 18:23:42 2022
 
-@author: jgbon
+@author: pieroot
 """
 
 from deepface import DeepFace
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QDesktopWidget, QVBoxLayout, QLabel, QFileDialog
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtGui import QIcon
-from camera import Camera
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+
+
+import urllib.request
+
+# from camera import Camera
+
+import sys
 import os
+
 from tkinter import filedialog
 from tkinter import messagebox
+
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import filedialog
 
 
 class MyApp(QWidget):
@@ -32,26 +45,55 @@ class MyApp(QWidget):
         btn.resize(btn.sizeHint())
         btn.clicked.connect(QCoreApplication.instance().quit)
 
-        img1_btn = QPushButton('이미지 선택', self)
-        # img1_btn.setFont()
-        img1_btn.resize(img1_btn.sizeHint())
+        select_img1 = QPushButton('원본 이미지 선택', self)
+        # select_img1.setFont()
+        select_img1.move(30, 5)
+        select_img1.resize(select_img1.sizeHint())
+        # select_img1.clicked.connect(self.setIMG())
+
+        select_img2 = QPushButton('비교할 이미지 선택', self)
+        # select_img2.setFont()
+        select_img2.move(170, 5)
+        select_img2.resize(select_img1.sizeHint())
+        # select_img2.clicked.connect(self)
 
         self.setWindowTitle('Face Project!!')
         self.setWindowIcon(QIcon('deepface-icon.png'))
-        self.move(300, 300)
-        self.resize(400, 200)
+        # self.move(300, 300)
+        self.resize(700, 500)
+        self.setIMG()
+        self.center()
         self.show()
 
-    def setIMG(self):
-        img1_name = "img1.jpg"
-        img2_name = "null"
-        list_file = []  # 파일 목록 담을 리스트 생성
-        files = filedialog.askopenfilenames(initialdir="/", title="파일을 선택 해 주세요", filetypes=(("*.xlsx", "*xlsx"), ("*.xls", "*xls"), ("*.csv", "*csv")))
-        result = DeepFace.verify(img1_path="img1.jpg", img2_path="img2.jpg")
-        print(result)
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
+    def setIMG(self):
+        pixmap1 = QPixmap('C:\\code\\python\\face\\img1.jpg')
+        lbl_img1 = QLabel()
+        # pixmap1 = pixmap1.scaledToWidth(80)
+        pixmap1 = pixmap1.scaledToHeight(360)
+        lbl_img1.setPixmap(pixmap1)
+
+        pixmap2 = QPixmap('img2.jpg')
+        lbl_img2 = QLabel()
+        # pixmap2 = pixmap2.scaledToWidth(80)
+        pixmap2 = pixmap2.scaledToHeight(120)
+        lbl_img2.setPixmap(pixmap2)
+
+        lbl_size = QLabel('Width: ' + str(pixmap1.width()) + ', Height: ' + str(pixmap1.height()))
+        lbl_size.setAlignment(Qt.AlignCenter)
+
+        vbox = QVBoxLayout()
+        lbl_img1.move(530, 530)
+        vbox.addWidget(lbl_img1)
+        vbox.addWidget(lbl_size)
+        self.setLayout(vbox)
 
 if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   ex = MyApp()
-   sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
