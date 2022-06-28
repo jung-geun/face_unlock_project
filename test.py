322f0d337637a2f2556476af57c2c45009d7425a
jung-geun <jgbong0306@gmail.com>
2022-06-28 22:39:10 +0900

    img layout setup

--- test.py
+++ test.py
@@ -7,13 +7,9 @@ Created on Fri Jun 24 18:23:42 2022
 
 from deepface import DeepFace
 
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QDesktopWidget, QVBoxLayout, QLabel, QFileDialog
-from PyQt5.QtGui import QFont
-from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtGui import QIcon
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
-
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QDesktopWidget, QVBoxLayout, QHBoxLayout, QLabel, QFileDialog, QProgressBar
+from PyQt5.QtGui import QFont, QIcon, QPixmap
+from PyQt5.QtCore import QCoreApplication, QBasicTimer, Qt
 
 import urllib.request
 
@@ -37,29 +33,10 @@ class MyApp(QWidget):
 
     def initUI(self):
         QToolTip.setFont(QFont('SansSerif', 10))
-        self.setToolTip('This is a <b>QWidget</b> widget')
-
-        btn = QPushButton('Quit', self)
-        # btn.setFont('<b>exit</b>')
-        btn.move(320, 5)
-        btn.resize(btn.sizeHint())
-        btn.clicked.connect(QCoreApplication.instance().quit)
-
-        select_img1 = QPushButton('원본 이미지 선택', self)
-        # select_img1.setFont()
-        select_img1.move(30, 5)
-        select_img1.resize(select_img1.sizeHint())
-        # select_img1.clicked.connect(self.setIMG())
-
-        select_img2 = QPushButton('비교할 이미지 선택', self)
-        # select_img2.setFont()
-        select_img2.move(170, 5)
-        select_img2.resize(select_img1.sizeHint())
-        # select_img2.clicked.connect(self)
+        self.setToolTip('This is a <b>Face unlock</b> widget')
 
         self.setWindowTitle('Face Project!!')
         self.setWindowIcon(QIcon('deepface-icon.png'))
-        # self.move(300, 300)
         self.resize(700, 500)
         self.setIMG()
         self.center()
@@ -72,25 +49,65 @@ class MyApp(QWidget):
         self.move(qr.topLeft())
 
     def setIMG(self):
+        quitbtn = QPushButton('Quit', self)
+        quitbtn.setToolTip('<b>exit</b>')
+        quitbtn.move(320, 5)
+        quitbtn.resize(quitbtn.sizeHint())
+        quitbtn.clicked.connect(QCoreApplication.instance().quit)
+
+        select_img1 = QPushButton('원본 이미지 선택', self)
+        # select_img1.setFont()
+        # select_img1.move(30, 5)
+        select_img1.resize(select_img1.sizeHint())
+        # select_img1.clicked.connect(self.setIMG())
+
+        select_img2 = QPushButton('비교할 이미지 선택', self)
+        # select_img2.setFont()
+        # select_img2.move(170, 5)
+        select_img2.resize(select_img1.sizeHint())
+        # select_img2.clicked.connect(self)
+
         pixmap1 = QPixmap('C:\\code\\python\\face\\img1.jpg')
         lbl_img1 = QLabel()
         # pixmap1 = pixmap1.scaledToWidth(80)
         pixmap1 = pixmap1.scaledToHeight(360)
         lbl_img1.setPixmap(pixmap1)
 
-        pixmap2 = QPixmap('img2.jpg')
+        pixmap2 = QPixmap('C:\\code\\python\\face\\img2.jpg')
         lbl_img2 = QLabel()
         # pixmap2 = pixmap2.scaledToWidth(80)
-        pixmap2 = pixmap2.scaledToHeight(120)
+        pixmap2 = pixmap2.scaledToHeight(360)
         lbl_img2.setPixmap(pixmap2)
 
-        lbl_size = QLabel('Width: ' + str(pixmap1.width()) + ', Height: ' + str(pixmap1.height()))
+        lbl_size = QLabel('img1 Width: ' + str(pixmap1.width()) + ', Height: ' + str(pixmap1.height()))
+        lbl_size2 = QLabel('img2 Width: ' + str(pixmap2.width()) + ', Height: ' + str(pixmap2.height()))
         lbl_size.setAlignment(Qt.AlignCenter)
+        lbl_size2.setAlignment(Qt.AlignCenter)
+
+        pbar = QProgressBar(self)
+        timer = QBasicTimer()
+        timer.start(100, self)
+
+        buttonbox = QHBoxLayout()
+        buttonbox.addWidget(select_img1)
+        buttonbox.addStretch(1)
+        buttonbox.addWidget(select_img2)
+        buttonbox.addStretch(2)
+        buttonbox.addWidget(quitbtn)
+        # buttonbox.addStretch(1)
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(lbl_img1)
+        hbox.addWidget(lbl_img2)
+        hbox.addStretch(1)
 
         vbox = QVBoxLayout()
-        lbl_img1.move(530, 530)
-        vbox.addWidget(lbl_img1)
+        vbox.addLayout(buttonbox)
+        vbox.addLayout(hbox)
         vbox.addWidget(lbl_size)
+        vbox.addWidget(lbl_size2)
+
         self.setLayout(vbox)
 
 if __name__ == '__main__':
